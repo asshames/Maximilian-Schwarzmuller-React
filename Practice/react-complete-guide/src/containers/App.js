5323b76e633490524629001a1aeb7c6a12575b6f
asshames <assamialomarshames@gmail.com>
2022-02-07 23:06:59 +0600

    create a Higher order comoponent and add in app.js

--- Practice/react-complete-guide/src/containers/App.js
+++ Practice/react-complete-guide/src/containers/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends Component {
 
@@ -88,7 +89,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass className={classes.App}>
         <Cockpit
           title={this.props.appTitle}
           showPersons={this.state.showPersons}
@@ -96,7 +97,7 @@ class App extends Component {
           clicked={this.togglePersonsHandler}
         />
         {persons}
-      </div>
+      </WithClass>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
