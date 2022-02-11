5ca12489b305e4d60f2577d5becf9143c3a4b3b8
asshames <assamialomarshames@gmail.com>
2022-02-12 05:30:53 +0600

    Implementing the backdrop Component

--- Burger Builder Project/burger-builder-project/src/components/UI/Modal/Modal.js
+++ Burger Builder Project/burger-builder-project/src/components/UI/Modal/Modal.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 import classes from './Modal.module.css';
 import Aux from '../../../hoc/Auxillary';
+import Backdrop from '../Backdrop/Backdrop';
 
 const modal = ( props ) => (
     <Aux>
+        <Backdrop show={props.show} clicked={props.modalClosed} />
         <div
             className={classes.Modal}
             style={{
