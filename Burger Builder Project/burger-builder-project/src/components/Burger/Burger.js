7ff2ba52a0f52ad21304bd268a93c9ba3cf2a760
asshames <assamialomarshames@gmail.com>
2022-02-11 17:48:26 +0600

    dinamic burger ingredient content build

--- Burger Builder Project/burger-builder-project/src/components/Burger/Burger.js
+++ Burger Builder Project/burger-builder-project/src/components/Burger/Burger.js
@@ -4,12 +4,19 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+
+    const transformIngredients = Object.keys(props.ingredients)
+        .map(igkey => {
+            return [...Array(props.ingredients[igkey])]
+                .map((_, i) => {
+                    return <BurgerIngredient key={igkey + i} type={igkey} />;
+                });
+        });
     
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
