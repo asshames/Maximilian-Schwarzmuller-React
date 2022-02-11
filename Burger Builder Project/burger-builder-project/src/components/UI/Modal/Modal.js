19e616bc2e62ac4e0918087443ae657c3b011d0e
asshames <assamialomarshames@gmail.com>
2022-02-12 05:15:50 +0600

    Showing and hiding the modal

--- Burger Builder Project/burger-builder-project/src/components/UI/Modal/Modal.js
+++ Burger Builder Project/burger-builder-project/src/components/UI/Modal/Modal.js
@@ -5,8 +5,13 @@ import Aux from '../../../hoc/Auxillary';
 
 const modal = ( props ) => (
     <Aux>
-        <div className={classes.Modal}>
-            {props.children}
+        <div
+            className={classes.Modal}
+            style={{
+                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity: props.show ? '1' : '0'
+            }}>
+                {props.children}
         </div>
     </Aux>
 );
--- Burger Builder Project/burger-builder-project/src/containers/BurgerBuilder/BurgerBuilder.js
+++ Burger Builder Project/burger-builder-project/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -29,7 +29,8 @@ class BurgerBuilder extends Component{
             meat: 0
         },
         totalPrice: 4,
-        purchasable: false
+        purchasable: false,
+        purchasing: false
     }
 
     updatePurchaseState(ingredients) {
@@ -74,6 +75,9 @@ class BurgerBuilder extends Component{
         this.updatePurchaseState(updatedIngredients);
     }
 
+    purchaseHandler = () => {
+        this.setState({ purchasing: true });
+    }
 
     render() {
 
@@ -87,7 +91,7 @@ class BurgerBuilder extends Component{
 
         return (
             <Aux>
-                <Modal>
+                <Modal show={this.state.purchasing}>
                     <OrderSummary ingredients={ this.state.ingredients }/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
@@ -96,6 +100,7 @@ class BurgerBuilder extends Component{
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
                     purchasable={this.state.purchasable}
+                    ordered={this.purchaseHandler}
                     price={this.state.totalPrice}/>
             </Aux>
         );
