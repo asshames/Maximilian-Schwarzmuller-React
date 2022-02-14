8c61417964906f80daedcd2c5e2a702cc68863e9
asshames <assamialomarshames@gmail.com>
2022-02-15 04:50:00 +0600

    NewPost content is create and add to blog component

--- Practice/project-with-ajax/src/containers/Blog/Blog.js
+++ Practice/project-with-ajax/src/containers/Blog/Blog.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import './Blog.css';
 import Post from '../../components/Post/Post';
+import NewPost from '../../components/NewPost/NewPost';
 
 class Blog extends Component {
     render () {
@@ -11,6 +12,9 @@ class Blog extends Component {
                     <Post />
                     <Post />
                     <Post />
+                </section>
+                <section>
+                    <NewPost />
                 </section>
             </div>
         );
