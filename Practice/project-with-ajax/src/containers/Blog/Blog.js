bb141a6dd4fa0acbf5c1860eb250fa4cebc51e6d
asshames <assamialomarshames@gmail.com>
2022-02-15 04:44:34 +0600

    A blog example of hhtp/ajax is created

---
+++ Practice/project-with-ajax/src/containers/Blog/Blog.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './Blog.css';
+
+class Blog extends Component {
+    render () {
+        return (
+            <div>
+                <section className="Blog">
+                    <p>Something will be written</p>
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
