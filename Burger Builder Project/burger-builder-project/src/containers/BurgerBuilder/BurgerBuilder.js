e1ff6f7194944aa2c46c2bd7806d8a0b5b8ead7f
asshames <assamialomarshames@gmail.com>
2022-02-16 20:21:25 +0600

    Sendind a Post request to the app

--- Burger Builder Project/burger-builder-project/src/containers/BurgerBuilder/BurgerBuilder.js
+++ Burger Builder Project/burger-builder-project/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -84,7 +85,29 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        // alert('You continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Shames',
+                address: {
+                    house: '#456',
+                    street: 'Noclue 1',
+                    zipcode: '56544',
+                    country:'Bangladesh'
+                },
+                email: 'test@test.com',
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     render() {
