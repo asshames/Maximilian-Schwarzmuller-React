a67ac05dff07ab7be56005e5c17be9fe3ff7db4c
asshames <assamialomarshames@gmail.com>
2022-02-18 02:18:42 +0600

    switching between the pages

--- Practice/project-ajax-router/src/containers/Blog/Blog.js
+++ Practice/project-ajax-router/src/containers/Blog/Blog.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 
 import { Route } from 'react-router-dom';
 
-import Posts from './Posts/Posts';
 import './Blog.css'; 
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
     render() {
@@ -21,6 +22,7 @@ class Blog extends Component {
                 <Route path="/new-post" exact render={() => <h1> Home 2</h1>} /> */}
 
                 <Route path='/' exact component={Posts} />
+                <Route path='/new-post' exact component={NewPost} />
 
             </div>
         );
