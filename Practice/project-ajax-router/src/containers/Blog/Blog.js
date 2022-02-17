74edaa71c007f7a04519aa144d1dc9d8a5cca0f4
asshames <assamialomarshames@gmail.com>
2022-02-18 04:05:01 +0600

    using switch to load a single route

--- Practice/project-ajax-router/src/containers/Blog/Blog.js
+++ Practice/project-ajax-router/src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { NavLink, Route } from 'react-router-dom';
+import { NavLink, Route, Switch } from 'react-router-dom';
 
 import './Blog.css'; 
 import Posts from './Posts/Posts';
@@ -33,11 +33,12 @@ class Blog extends Component {
                 </header>       
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/new-post" exact render={() => <h1> Home 2</h1>} /> */}
-
+                    
                 <Route path='/' exact component={Posts} />
-                <Route path='/new-post' exact component={NewPost} />
-                <Route path='/:id' exact component={FullPost} />
-
+                <Switch>
+                    <Route path='/new-post' exact component={NewPost} />
+                    <Route path='/:id' exact component={FullPost} />
+                </Switch>
             </div>
         );
     }
