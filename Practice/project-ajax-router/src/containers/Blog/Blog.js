c62088aa7a166b31ed373c06e73000eaeb4f4e87
asshames <assamialomarshames@gmail.com>
2022-02-18 03:44:30 +0600

    Passing Route Parametes

--- Practice/project-ajax-router/src/containers/Blog/Blog.js
+++ Practice/project-ajax-router/src/containers/Blog/Blog.js
@@ -5,6 +5,7 @@ import { NavLink, Route } from 'react-router-dom';
 import './Blog.css'; 
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
     render() {
@@ -35,6 +36,7 @@ class Blog extends Component {
 
                 <Route path='/' exact component={Posts} />
                 <Route path='/new-post' exact component={NewPost} />
+                <Route path='/:id' exact component={FullPost} />
 
             </div>
         );
