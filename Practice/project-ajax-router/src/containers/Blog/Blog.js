d56bc935ca93fd1298bdb5e0ce067fe0b9b70dd0
asshames <assamialomarshames@gmail.com>
2022-02-18 02:14:09 +0600

    Rendering Components for Routes

--- Practice/project-ajax-router/src/containers/Blog/Blog.js
+++ Practice/project-ajax-router/src/containers/Blog/Blog.js
@@ -17,8 +17,11 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>       
-                <Route path="/" exact render={() => <h1>Home</h1>} />
-                <Route path="/new-post" exact render={() => <h1> Home 2</h1>} />
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/new-post" exact render={() => <h1> Home 2</h1>} /> */}
+
+                <Route path='/' exact component={Posts} />
+
             </div>
         );
     }
