bd1918bc53a05577b6ee22b74fcaf30bccbbe577
asshames <assamialomarshames@gmail.com>
2022-02-19 04:31:11 +0600

    Navigating to the Checkout

--- Burger Builder Project/burger-builder-project/src/containers/BurgerBuilder/BurgerBuilder.js
+++ Burger Builder Project/burger-builder-project/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -35,6 +35,7 @@ class BurgerBuilder extends Component{
     }
 
     componentDidMount() {
+        console.log(this.props);
         axios.get('https://my-burger-builder-78b40-default-rtdb.firebaseio.com/ingredients.json')
             .then(response => {
                 this.setState({ ingredients: response.data });
@@ -96,29 +97,32 @@ class BurgerBuilder extends Component{
 
     purchaseContinueHandler = () => {
         // alert('You continue!');
-        this.setState({ loading: true });
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice.toFixed(2),
-            customer: {
-                name: 'Shames',
-                address: {
-                    house: '#456',
-                    street: 'Noclue 1',
-                    zipcode: '56544',
-                    country:'Bangladesh'
-                },
-                email: 'test@test.com',
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false, purchasing: false});
-            })
-            .catch(error => {
-                this.setState({ loading: false, purchasing: false });
-            });
+        // this.setState({ loading: true });
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice.toFixed(2),
+        //     customer: {
+        //         name: 'Shames',
+        //         address: {
+        //             house: '#456',
+        //             street: 'Noclue 1',
+        //             zipcode: '56544',
+        //             country:'Bangladesh'
+        //         },
+        //         email: 'test@test.com',
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({ loading: false, purchasing: false});
+        //     })
+        //     .catch(error => {
+        //         this.setState({ loading: false, purchasing: false });
+        //     });
+
+        this.props.history.push('/checkout');
+        
     }
 
     render() {
