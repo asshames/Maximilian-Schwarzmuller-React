47950370ad7590e85ce634694ef334bc0c7cad20
asshames <assamialomarshames@gmail.com>
2022-02-19 16:42:10 +0600

    Fetching Orders

--- Burger Builder Project/burger-builder-project/src/containers/Orders/Orders.js
+++ Burger Builder Project/burger-builder-project/src/containers/Orders/Orders.js
@@ -1,9 +1,35 @@
 import React, { Component } from "react";
+import axios from "../../axios-orders";
 
 import Order from '../../components/Order/Order';
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
 
 class Orders extends Component{
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(res => {
+                // console.log(res.data);
+                const fetchingOrders = [];
+                for (let key in res.data) {
+                    fetchingOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                this.setState({ loading: false, orders: fetchingOrders });
+            })
+            .catch(error => {
+                // console.log(error);
+                this.setState({ loading: false });
+        })
+    }
+
     render() {
         return (
             <div>
@@ -14,4 +40,4 @@ class Orders extends Component{
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
