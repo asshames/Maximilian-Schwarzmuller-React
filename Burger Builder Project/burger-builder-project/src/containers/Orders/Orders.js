7a5137b54cf098bb536a9d736acbbd09ced2e7f0
asshames <assamialomarshames@gmail.com>
2022-02-19 16:13:47 +0600

    implementing Navigation links

--- Burger Builder Project/burger-builder-project/src/containers/Orders/Orders.js
+++ Burger Builder Project/burger-builder-project/src/containers/Orders/Orders.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import Order from '../../components/Order/Order';
 
 
-class Order extends Component{
+class Orders extends Component{
     render() {
         return (
             <div>
@@ -14,4 +14,4 @@ class Order extends Component{
     }
 }
 
-export default Order;
+export default Orders;
