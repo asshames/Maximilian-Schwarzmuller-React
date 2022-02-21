9a7aa0cb1802a16b6cee27749f8512fc0b332110
asshames <assamialomarshames@gmail.com>
2022-02-22 03:51:45 +0600

    fixing an error

--- Burger Builder Project/burger-builder-project/src/containers/Checkout/ContactData/ContactData.js
+++ Burger Builder Project/burger-builder-project/src/containers/Checkout/ContactData/ContactData.js
@@ -86,6 +86,7 @@ class ContactData extends Component {
                     ]
                 },
                 value: '',
+                validation: {},
                 valid: true
             }
         },
@@ -118,6 +119,10 @@ class ContactData extends Component {
     checkValidity(value, rules) {
         let isValid = true;
 
+        if (!rules) {
+            return true;
+        }
+
         if (rules.required) {
             isValid = value.trim() !== ''  && isValid;
         }
@@ -145,7 +150,7 @@ class ContactData extends Component {
         updatedFormElement.touched = true;
         updatedOrderForm[inputIdentifier] = updatedFormElement;
         
-        const updatedFormIsValid = true;
+        let updatedFormIsValid = true;
         for (let inputIdentifier in updatedOrderForm) {
             updatedFormIsValid = updatedOrderForm[inputIdentifier].valid && updatedFormIsValid;
         }
