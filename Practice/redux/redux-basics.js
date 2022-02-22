8d176ed7cd6576b641e51c9fb912b0715a44dfd5
asshames <assamialomarshames@gmail.com>
2022-02-22 17:09:01 +0600

    setting up Reducer and Store

--- Practice/redux/redux-basics.js
+++ Practice/redux/redux-basics.js
@@ -1,1 +1,19 @@
 const redux = require('redux');
+
+const createStore = redux.createStore;
+
+const initialState = {
+    couter: 0
+}
+
+//reducer
+
+const rootReducer = (state = initialState, action) => {
+    return state;
+}
+
+//Store
+
+const store = createStore(rootReducer);
+
+console.log(store.getState());
