551a1317acb59241b9ebe21e0724f5113309999f
asshames <assamialomarshames@gmail.com>
2022-02-22 17:21:00 +0600

    dispatching actions

--- Practice/redux/redux-basics.js
+++ Practice/redux/redux-basics.js
@@ -9,11 +9,28 @@ const initialState = {
 //reducer
 
 const rootReducer = (state = initialState, action) => {
+    if (action.type === 'INC_COUNTER') {
+        return {
+            ...state,
+            couter: state.couter + 1
+        }
+    }
+    if (action.type === 'ADD_COUNTER') {
+        return {
+            ...state,
+            couter: state.couter + action.value
+        }
+    }
     return state;
 }
 
 //Store
 
 const store = createStore(rootReducer);
+console.log(store.getState());
 
+//Dispatching action
+
+store.dispatch({ type: 'INC_COUNTER' });
+store.dispatch({ type: 'ADD_COUNTER', value: 10 });
 console.log(store.getState());
