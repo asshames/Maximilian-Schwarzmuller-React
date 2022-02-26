82e47a0152ae9b77d43a1a5f697cca30280e78d1
asshames <assamialomarshames@gmail.com>
2022-02-27 04:26:09 +0600

    A leanear Switch case statement

--- Practice/redux-advanced/src/store/reducers/result.js
+++ Practice/redux-advanced/src/store/reducers/result.js
@@ -5,6 +5,11 @@ const initialState = {
     results: []
 };
 
+const deleteResult = ( state, action ) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+    return updateObject( state, { results: updatedArray } );
+};
+
 const reducer = ( state = initialState, action ) => {
     switch (action.type) {
         //update result data
@@ -13,9 +18,8 @@ const reducer = ( state = initialState, action ) => {
         case actionTypes.DELETE_RESULT:
             // const id = 2;
             // const newArray = [...state.results];
-            // newArray.splice(id, 1)
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return updateObject(state, { results: updatedArray });
+            // newArray.splice(id, 1);
+            return deleteResult();
     }
     return state;
 };
