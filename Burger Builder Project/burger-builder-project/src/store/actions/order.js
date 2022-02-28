bde84011dc9e1ca09e83f827c8197999d27cf507
asshames <assamialomarshames@gmail.com>
2022-03-01 03:45:59 +0600

    adding order Actions

---
+++ Burger Builder Project/burger-builder-project/src/store/actions/order.js
@@ -0,0 +1,30 @@
+import * as actionsTypes from "./actionTypes";
+import axios from "../../axios-orders";
+
+export const purchaseBurgerSuccess = (id, orderData) => {
+    return {
+        type: actionsTypes.PURCHASE_BURGER_SUCCESS,
+        orderId: id,
+        orderData: orderData
+    };
+};
+
+export const purchaseBurgerFail = (error) => {
+    return {
+        type: actionsTypes.PURCHASE_BURGER_FAIL,
+        error: error
+    };
+};
+
+export const purchaseBurgerStart = (orderData) => {
+    return dispatch => {
+        axios.post('/orders.json', orderData)
+            .then(response => {
+                console.log(response.data);
+                dispatch(purchaseBurgerSuccess(response.data, orderData));
+            })
+            .catch(error => {
+                dispatch(purchaseBurgerFail(error));
+            });
+    };
+};
