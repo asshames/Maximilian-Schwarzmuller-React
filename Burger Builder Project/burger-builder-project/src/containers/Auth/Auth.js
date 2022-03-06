2c68b841a1029b824bc3c999a7b6c74819a004da
asshames <assamialomarshames@gmail.com>
2022-03-06 23:58:19 +0600

    adding spinner into the auth.js

--- Burger Builder Project/burger-builder-project/src/containers/Auth/Auth.js
+++ Burger Builder Project/burger-builder-project/src/containers/Auth/Auth.js
@@ -5,6 +5,7 @@ import Button from '../../components/UI/Button/Button';
 import classes from './Auth.module.css';
 import * as actions from '../../store/actions/index';
 import { connect } from 'react-redux';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Auth extends Component {
     state = {
@@ -115,7 +116,11 @@ class Auth extends Component {
                 shouldValidate={formElement.config.validation}
                 touched={formElement.config.touched}
                 changed={( event ) => this.inputChangedHandler( event, formElement.id )} />
-        ) );
+        ));
+        
+        if (this.props.loading) {
+            form = <Spinner />
+        }
 
         return (
             <div className={classes.Auth}>
@@ -130,10 +135,17 @@ class Auth extends Component {
     }
 };
 
+const mapStateToProps = state => {
+    return {
+        loading: state.auth.loading
+    };
+};
+
+
 const mapDispatchToProps = dispatch => {
     return {
         onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
     };
 };
 
-export default connect (null, mapDispatchToProps) (Auth);
+export default connect (mapStateToProps, mapDispatchToProps) (Auth);
